8a96f0209623d29b02eaca74a6f67ffbf6ce0c10
Rolan Eleazer T Molano <molanorolan778@gmail.com>
2016-07-14 20:43:01 +0800

    try

---
+++ Switch_Ranier.cpp
@@ -0,0 +1,158 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main()
+{
+	int password;
+password = 2468;
+
+
+string username;
+string ranier ;
+
+
+
+
+cout << "Enter username:";
+cin >> ranier;
+
+cout <<"\n";
+
+cout << "Enter password:";
+cin >> password;
+
+cout <<"\n";
+
+
+if (password == 2468)
+{
+cout << "Access Granted...";
+}
+else
+exit(0);
+system("cls");
+
+int i;
+                      for(i=0;i<1000+100;i+=100)
+                      {
+                     ;
+                     cout<<"#";
+                     Sleep(1000);
+                     }
+
+
+system("cls");
+int n;
+cout<<"\tMAIN MENU"<<endl;
+cout<<"     ONLINE ACTIVITY 3"<<endl;
+cout<<"1. For loop vertical \n" "2. For loop horizontal \n" "3. While loop vertical \n" "4. While loop horizontal \n" "5. Edi Wow For loop vertical \n" "6. Edi wow For loop horizontal \n" "7. Edi Wow while loop vertical \n" "8. Edi Wow while loop horizontal \n\n" "Enter Input:";
+cin>>n;
+
+switch (n){
+       
+case 1:
+     system("cls");
+     	int a;
+	for(a=2;a<=101;a+=1)
+	{
+		cout<<a<<""<<endl;
+		Sleep(1000);
+	}
+	system("pause");
+	return(0);
+          
+            
+          
+
+case 2:
+     
+ system("cls");
+      int e;
+	for(e=2;e<=101;e+=1)
+	{
+		cout<<e<<" ";
+		Sleep(1000);
+	}
+	system("pause");
+    return(0);   
+{
+case 3:
+	system("cls");
+     int d=0;
+	while (d<=49)
+	{
+		d+=1;
+		cout<<d<<""<<endl;
+		Sleep(1000);
+	}
+	system("pause");
+    return(0);   
+}
+{
+case 4:
+	system("cls");
+	int u=0;
+	while (u<=49)
+	{
+		u+=1;
+		cout<<u<<" ";
+		Sleep(1000);
+	}
+	system("pause");
+    return(0);
+}
+{
+case 5:
+	system("cls");
+     	int h;
+	for(h=2;h<=100;h+=1)
+	{
+		cout<<"edi wow "<<h<<endl;
+		Sleep(1000);
+	}
+	system("pause");
+	return(0);  
+}
+{
+case 6:
+	system("cls");
+     	int q;
+	for(q=2;q<=100;q+=1)
+	{
+		cout<<"edi wow "<<q;
+		Sleep(1000);
+	}
+	system("pause");
+	return(0);
+}
+{
+case 7:
+	system("cls");
+     	int i=0;
+	while (i<=99)
+	{
+		i+=1;
+		cout<<"Edi Wow "<<a<<endl;
+		Sleep(1000);
+	}
+	system("pause");
+	return(0);
+}
+{
+case 8:
+		system("cls");
+     	int o=0;
+	while (o<=99)
+	{
+		o+=1;
+		cout<<"Edi Wow "<<o;
+		Sleep(1000);
+	}
+	system("pause");
+	return(0);
+}
+	system("pause");
+    return(0);   
+}
+}
+
